cdb3b463ffc87b10ff2f1479b954fe4f20c0a1b3
rachel-greenlee <56199620+rachel-greenlee@users.noreply.github.com>
2021-11-10 18:50:05 -0700

    in-class HW6 lab files completed

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,20 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+        
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
 
+    const line = d3.line()
+        .x(d => xScale(+d.index))
+        .y(d => yScale(+d.Agriculture));
+    
     d3.select('#answer1')
-        // append more elements here
+        .append('path')
+        .attr('d', line(data))
+        .attr('stroke', '#474a4f')
 
 });
